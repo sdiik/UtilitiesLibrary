857fe62ee37ffb20aebd5dcc097bf3822b0b5db0
asani <asani@asanis-MacBook-Pro-2.local>
2024-07-31 09:18:04 +0700

    base code library

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "UtilitiesLibrary",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "UtilitiesLibrary",
+            targets: ["UtilitiesLibrary"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "UtilitiesLibrary"),
+        .testTarget(
+            name: "UtilitiesLibraryTests",
+            dependencies: ["UtilitiesLibrary"]),
+    ]
+)
---
+++ Sources/UtilitiesLibrary/UtilitiesLibrary.swift
@@ -0,0 +1,7 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+
+public struct Utilities {
+    var baseURL = "www.google.com"
+}
